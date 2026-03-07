620757081fbc1d2fddef54ff5073a7dec96d3641
pablobeckg <beckgarreaud@gmail.com>
2026-03-07 01:53:23 +0100

    Configuración final para Cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import adapter from '@sveltejs/adapter-cloudflare'; // <--- ESTE DEBE SER EL IMPORT
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter() // Sin parámetros
+		adapter: adapter()
 	}
 };
 
