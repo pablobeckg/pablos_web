f92408093e4d145e11e847c82abe31fa83a951cd
pablobeckg <beckgarreaud@gmail.com>
2026-03-07 01:17:13 +0100

    hola

---
+++ svelte.config.js
@@ -0,0 +1,15 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/pablos_web' : '',
+		}
+	}
+};
+
+export default config;
