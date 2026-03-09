7842e567f3501a2b1b68529096b8595c345aea5a
pablobeckg <beckgarreaud@gmail.com>
2026-03-09 02:46:46 +0100

    mas

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -31,8 +31,7 @@
 </svelte:head>
 
 <header>
-  <h1>ZITO</h1>
-
+  <h1><a href="https://zito.pages.dev">ZITO</a></h1>
   <nav class="navbar">
     <a href="/">Home</a>
     <a href="/sets">Sets</a>
@@ -50,11 +49,11 @@
     id="sc-widget"
     title="Reproductor de SoundCloud"
     width="100%"
-    height="166"
+    height="176"
     scrolling="no"
     frameborder="no"
     allow="autoplay"
-    src={`https://w.soundcloud.com/player/?url=${encodeURIComponent($currentTrackUrl)}&auto_play=${$shouldAutoplay}&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&show_artwork=false&continuous_play=false`}
+    src={`https://w.soundcloud.com/player/?url=${encodeURIComponent($currentTrackUrl)}&auto_play=${$shouldAutoplay}&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&show_artwork=false&continuous_play=false&color=%231b1b1b&visual=false`}
   >
   </iframe>
 </footer>
@@ -63,25 +62,38 @@
   :global(body) {
     margin: 0;
     font-family: 'Arial', sans-serif;
+    background-color: #f5f5f5;
   }
 
   header {
+    clip-path: polygon(1% 5%, 100% 0%, 98% 60%, 1.5% 100%);
+    margin: 1rem 2rem 0 2rem;
     display: flex;
     position: fixed;
-    width: 100%;
-    height: 3rem;
+    width: calc(100% - 4rem);
+    height: 4rem;
     z-index: 1000;
     align-items: center;
+    box-shadow: 0 0 1rem rgb(0, 0, 0, 0.8);
   }
 
-  header h1 {
-    margin: 1rem 2rem;
+  header h1 a {
+    background-color: #f5f5f5;
+    padding: 1rem 2rem;
+    color: #1b1b1b;
+    text-decoration: none;
+    font-weight: bold;
+    transition: color 0.2s;
+  }
+  header h1 a:hover {
+    
+        color: #ffcc00;
   }
 
   .navbar {
     display: flex;
     background-color: #1b1b1b;
-    padding: 1rem 2rem;
+    padding-left: 2rem;
     gap: 2rem;
     width: 100%;
     height: 100%;
@@ -100,22 +112,23 @@
   }
 
   main {
-    padding: 5rem 0rem 0rem 0rem;
-    background-color: #f1f1f1;
-    height: 100%;
-    padding-bottom: 90px;
+    padding: 6rem 2rem 2rem 2rem;
+    background-color: #f5f5f5;
     color: #1b1b1b;
+    height: 900px;
   }
 
   footer.player {
+    
     position: fixed;
     bottom: 0;
     left: 0;
     width: 100%;
     height: 3rem;
-    background: #111;
-    box-shadow: 0 -5px 20px rgba(0, 0, 0, 0.5);
     display: flex;
     align-items: center;
+  }
+  iframe {
+    border-top-right-radius: 20px;
   }
 </style>
